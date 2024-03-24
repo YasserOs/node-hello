b462df76208eff06402a20545f3f1f9840f4b816
YasserOs <yasserosm.yo@gmail.com>
2024-03-24 18:50:30 +0200

    added ecs task definition , execution role

---
+++ terraform/ecs.tf
@@ -0,0 +1,46 @@
+resource "aws_ecs_task_definition" "app_task" {
+  family                   = "Nodejs-Hello-world" # Name your task
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "hello-world",
+      "image": "${aws_ecr_repository.app_ecr_repo.repository_url}",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 3000,
+          "hostPort": 3000
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"] # use Fargate as the launch type
+  network_mode             = "awsvpc"    # add the AWS VPN network mode as this is required for Fargate
+  memory                   = 512         # Specify the memory the container requires
+  cpu                      = 256         # Specify the CPU the container requires
+  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+}
+
+
+resource "aws_ecs_service" "app_service" {
+  name            = "nodejs-service"     # Name the service
+  cluster         = "${aws_ecs_cluster.my_cluster.id}"   # Reference the created Cluster
+  task_definition = "${aws_ecs_task_definition.app_task.arn}" # Reference the task that the service will spin up
+  launch_type     = "FARGATE"
+  desired_count   = 3 # Set up the number of containers to 3
+
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.target_group.arn}" # Reference the target group
+    container_name   = "${aws_ecs_task_definition.app_task.family}"
+    container_port   = 3000 # Specify the container port
+  }
+
+  network_configuration {
+    subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}"]
+    assign_public_ip = false     
+    security_groups  = ["${aws_security_group.service_security_group.id}"] # Set up the security group
+  }
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -5,3 +5,8 @@ resource "aws_ecr_repository" "app_ecr_repo" {
 resource "aws_ecs_cluster" "my_cluster" {
   name = "nodejs-fargate-cluster" # Name your cluster here
 }
+
+
+output "app_url" {
+  value = aws_alb.application_load_balancer.dns_name
+}
