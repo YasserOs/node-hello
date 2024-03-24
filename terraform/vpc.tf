c6062e86e7842277eeae2506738f8be78b47b925
YasserOs <yasserosm.yo@gmail.com>
2024-03-24 19:08:44 +0200

    added loadbalancer and seucrity groups

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,14 +1,13 @@
 resource "aws_default_vpc" "default_vpc" {
-  name = default-vpc
 }
 
 resource "aws_default_subnet" "default_subnet_a" {
-  name = default-subneta
+  
   availability_zone = "us-east-2a"
 }
 
 resource "aws_default_subnet" "default_subnet_b" {
-  name= default-subnetb
+  
   availability_zone = "us-east-2b"
 }
 
