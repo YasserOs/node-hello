b2b812fe934f69dc28da4aed5eec30114bf60fd8
YasserOs <YasserOs@users.noreply.github.com>
2024-03-24 19:49:59 +0000

    Apply automatic changes

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -14,7 +14,7 @@ resource "aws_ecs_task_definition" "app_task" {
   [
     {
       "name": "Nodejs-Hello-world",
-      "image": "${aws_ecr_repository.app_ecr_repo.repository_url}:TAG",
+      "image": "${aws_ecr_repository.app_ecr_repo.repository_url}:95742c1",
       "essential": true,
       "portMappings": [
         {
