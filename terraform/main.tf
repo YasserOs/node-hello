d92ddebaed49208211d3f7895336d5df74016097
YasserOs <yasserosm.yo@gmail.com>
2024-03-24 17:31:20 +0200

    added ecs cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,7 @@
-# main.tf
 resource "aws_ecr_repository" "app_ecr_repo" {
   name = "nodejs-app-repo"
+}
+
+resource "aws_ecs_cluster" "my_cluster" {
+  name = "nodejs-fargate-cluster" # Name your cluster here
 }
