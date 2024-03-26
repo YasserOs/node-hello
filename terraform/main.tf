b7e4668dfddc3710ed526364c366af2e24e3b4f8
YasserOs <yasserosm.yo@gmail.com>
2024-03-26 21:56:08 +0200

    reverting back changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,32 +3,3 @@ output "app_url" {
   value = aws_alb.application_load_balancer.dns_name
 }
 
-resource "aws_s3_bucket" "bucket" {
-    bucket = "yosm-terraform-state-bucket"
-    versioning {
-        enabled = true
-    }
-    server_side_encryption_configuration {
-        rule {
-            apply_server_side_encryption_by_default {
-                sse_algorithm = "AES256"
-            }
-        }
-    }
-    object_lock_configuration {
-        object_lock_enabled = "Enabled"
-    }
-}
-
-resource "aws_dynamodb_table" "terraform-lock" {
-    name           = "terraform_state_lock_table"
-    attribute {
-        name = "LockID"
-        type = "S"
-    }
-    read_capacity  = 5
-    write_capacity = 5
-    hash_key       = "LockID"
-    
-
-}
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -9,13 +9,14 @@ terraform {
   required_version = ">= 1.2.0"
   
   backend "s3" {
-    bucket         	   = "yosm-terraform-state-bucket"
-    key              	   = "terraform.tfstate"
+    bucket         	   = "yosm-terraform-tfstate"
+    key              	   = "state/terraform.tfstate"
     region         	   = "us-east-2"
     encrypt        	   = true
-    dynamodb_table = "terraform_state_lock_table"
+    dynamodb_table = "terraform-tfstate-lock"
   }
 }
+
 provider "aws" {
   region  = "us-east-2"
 }
