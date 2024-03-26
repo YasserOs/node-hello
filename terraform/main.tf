7bad3b0d18c4c9796d955198640cc481424a7786
YasserOs <yasserosm.yo@gmail.com>
2024-03-26 21:41:38 +0200

    creating s3 and dynamodb table

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,30 @@ output "app_url" {
   value = aws_alb.application_load_balancer.dns_name
 }
 
+resource "aws_s3_bucket" "bucket" {
+    bucket = "yosm-terraform-state-bucket"
+    versioning {
+        enabled = true
+    }
+    server_side_encryption_configuration {
+        rule {
+            apply_server_side_encryption_by_default {
+                sse_algorithm = "AES256"
+            }
+        }
+    }
+    object_lock_configuration {
+        object_lock_enabled = "Enabled"
+    }
+}
+
+resource "aws_dynamodb_table" "terraform-lock" {
+    name           = "terraform_state_lock_table"
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+    hash_key       = "LockID"
+    
+
+}
